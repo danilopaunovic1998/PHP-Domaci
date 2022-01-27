8e46463de23b84255609d0314e43a70ad37477fb
danilopaunovic1998 <57719918+danilopaunovic1998@users.noreply.github.com>
2022-01-27 14:50:34 +0100

    Kreiranje user klase

---
+++ model/user.php
@@ -0,0 +1,24 @@
+<?php
+
+    class User {
+        public $id;
+        public $username;
+        public $password;
+        public $email;
+
+        public function __construct($id=null,$username=null,$password=null,$email=null)
+        {
+            $this->id = $id;
+            $this->username = $username;
+            $this->password = $password;
+            $this->email = $email;
+        }
+
+        public function logInUser($email, $password, mysqli $conn)
+        {
+            $query = "SELECT * from user where email = $email and password = $password";
+            //konekcija sa bazom
+        }
+    }
+
+?>
